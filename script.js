2a52c6fe291baf3434007c04f0f3acb1353e884e
admin <admin@admins-MacBook-Pro.local>
2019-01-04 20:49:36 -0800

    upgrade css, add results rendering function

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ let score = 0;
 let questionNumber = 0;
 
 function updateScore() {
-    //should increase score by 1 if answer was correct
-    score++
-    $('.js-score').text(score)
-}
+    // increse score by 1 and renders it
+    score++;
+    $('.js-score').text(score);
+};
 
 function updateQuestionNumber() {
-    //increse questionNumber by 1 
-    questionNumber++
-    //update html
-    $('.js-question-number').text(questionNumber)
-}
+    //increse questionNumber by 1 and renderes it
+    questionNumber++;
+    $('.js-question-number').text(questionNumber);
+};
 
 function generateQuestion() {
-    //generate questions html
+    //generates new question
     return `
     <h1>${STORE[questionNumber].question}</h1>
     <form class="question-from">
@@ -36,28 +35,27 @@ function generateQuestion() {
             <button type="submit" class="js-submit-btn btn">Submit</button>
         </fieldset>
     </form>`
-}
+};
 
 function startQuiz() {
-    //listen for submit btn clicks
-    //remove start-window when clicked
+    // listen for start btn click => removes start-window when clicked
     $('.js-start-btn').on('click', function(event){
         $(this).parent().remove();
         renderQuestion();
-        $('.stats').removeClass('hidden')
-    })
-}
+        $('.stats').removeClass('hidden');
+    });
+};
 
 function renderQuestion() {
-    //render question
+    // renders a question, checks answer
     $('.js-question-window').html(generateQuestion())
     updateQuestionNumber();
     checkAnswer();
 }
 
 function checkAnswer() {
-    //compare submited value vs obj.correctAnswer
-
+    // compares submited value vs obj.correctAnswer
+    // shows feedback on selected answer
     $('form').on('submit', function(event) {
         event.preventDefault();
         if($('input:checked').val()===STORE[questionNumber-1].correctAnswer){
@@ -66,47 +64,66 @@ function checkAnswer() {
         } else {
             showWrong();
         }
-
-        // generateQuestion();
-        // renderQuestion();
-    })
-}
+    });
+};
 
 function renderNextWindow() {
     $('.js-btn-next').on('click', function(event){
-        // renders finish window
+        // renders results
         if (questionNumber===10){
-            $('.js-question-window').html(`<h1>FINISH!</h1>
-            <button type="button" class="js-btn-restart btn btn-1">Next</button>`);
-            restartQuiz()   
-        // renders next question window          
+            $('.js-question-window').html(results());
+            restartQuiz();   
+        // renders next question          
         } else {
-            generateQuestion()
-            renderQuestion()    
+            generateQuestion();
+            renderQuestion();    
         }
-    })
-}
+    });
+};
+
+function results() {
+    // generates results
+    if (score>=8){
+        return `<div class="result-icon"><img src="assets/images/starks.png" alt="stark house icon"/></div>
+        <h1>EXCELLENT!</h1>
+        <p>Your score is ${score} out of 10!</p>
+        <p>Excellent job refreshing your knowlege before watching the last season!</p><button type="button" class="js-btn-restart btn btn-1">Restart</button>`
+    } else if (8>score>=4) {
+        return `<div class="result-icon"><img src="assets/images/targaryen.png" alt="targaryen house icon"/></div>
+        <h1>Well done!</h1>
+        <p>Your score is ${score} out of 10!</p>
+        <p>Keep on practicing to become a truth Stark!</p><button type="button" class="js-btn-restart btn btn-1">Restart</button>`
+    } else if (score<4) {
+        return `<div class="result-icon"><img src="assets/images/baratheon.png" alt="baratheon house icon"/></div>
+        <h1>Shame on you!</h1>
+        <p>Your score is ${score} out of 10!</p>
+        <p>You'd better watching GOT now!</p><button type="button" class="js-btn-restart btn btn-1">Restart</button>`
+    }
+};
 
 function showCorrect() {
-    $('.js-question-window').html(`<div class="answer-icon"><img src="assets/images/starks.png" alt="correct answer icon"/></div>
+    // if answer is correct, generates feedback
+    $('.js-question-window').html(`<div class="answer-icon"><img src="assets/images/starks.png" alt="stark house icon"/></div>
     <h1>Correct!</h1>
     <p>Keep it up Young Wolf!</p>
     <button type="button" class="js-btn-next btn btn-1">Next</button>`);
-    renderNextWindow()
+    renderNextWindow();
 }
 
 function showWrong() {
-    $('.js-question-window').html(`<div class="answer-icon"><img src="assets/images/boltons.png" alt="correct answer icon"/></div>
+    // generates feedback if wrong
+    $('.js-question-window').html(`<div class="answer-icon"><img src="assets/images/boltons.png" alt="bolton house icon"/></div>
     <h1>Wrong!</h1>
     <p>The correct answer is: <span class="correct-answer">"${STORE[questionNumber-1].correctAnswer}"</span>
     <button type="button" class="js-btn-next btn btn-1">Next</button>`);
-    renderNextWindow()
-}
+    renderNextWindow();
+};
 
 function restartQuiz() {
+    // restarts page
     $('.js-btn-restart').on('click', function(event){
-        location.reload()
-    })
-}
+        location.reload();
+    });
+};
 
 $(startQuiz)
